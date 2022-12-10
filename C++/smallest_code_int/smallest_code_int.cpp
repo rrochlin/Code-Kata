255c9a1375cae3cc50c99212a896728d9c7dc505
Robert <rrochlin@yahoo.com>
2022-12-09 19:15:22 -0800

    adding challenge

--- C++/smallest_code_int/smallest_code_int.cpp
+++ C++/smallest_code_int/smallest_code_int.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+// https://www.codewars.com/kata/526156943dfe7ce06200063e/train/cpp
 std::string brainLuck(const std::string &code, const std::string &input) {
   return "";
 }
