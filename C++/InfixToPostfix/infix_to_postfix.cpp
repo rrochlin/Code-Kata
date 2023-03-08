c38d851b2d6bfa38ffa46f4374aca869fc46cdfc
Robert <rrochlin@yahoo.com>
2023-03-07 16:40:04 -0800

    adding gray code challenge to c++

--- C++/InfixToPostfix/infix_to_postfix.cpp
+++ C++/InfixToPostfix/infix_to_postfix.cpp
@@ -1,10 +1,21 @@
 #include <string>
 #include <iostream>
+#include <algorithm>
 
+
+// TODO need to actually parse array to confirm there are no parenthesis
 void stack_dump(std::string &result, std::string &op_stack){
-    std::reverse(op_stack.begin(), op_stack.end());
-    result += op_stack;
-    op_stack = "";
+    if (op_stack.find('(') != std::string::npos) {
+        std::size_t found = op_stack.find_last_of('(');
+        std::string inner = op_stack.substr(found+1);
+        std::reverse(inner.begin(), inner.end());
+        result += inner;
+        op_stack.erase(found+1);
+    } else {
+        std::reverse(op_stack.begin(), op_stack.end());
+        result += op_stack;
+        op_stack = "";
+    }
 }
 
 std::string to_postfix(std::string infix) {
@@ -23,35 +34,22 @@ std::string to_postfix(std::string infix) {
                 break;
             case '^': op_stack += chr; break;
             case '*': 
-                if (op_stack.back() != '+' | op_stack.back() != '-'){
-                    stack_dump(result, op_stack);
-                }
+                if (op_stack.back() == '+' & op_stack.back() == '-') stack_dump(result, op_stack);
                 op_stack += chr; break;
-            op_stack += chr; break;
             case '/': 
-                if (op_stack.back() != '+' | op_stack.back() != '-'){
-                    std::reverse(op_stack.begin(), op_stack.end());
-                    result += op_stack;
-                    op_stack = "";
-                }
+                if (op_stack.back() != '+' & op_stack.back() != '-') stack_dump(result, op_stack);
                 op_stack += chr; break;
             case '+': 
-                if (!op_stack.empty()){
-                    std::reverse(op_stack.begin(), op_stack.end());
-                    result += op_stack;
-                    op_stack = "";
-                }
+                if (!op_stack.empty() & op_stack.back() != '(') stack_dump(result, op_stack);
                 op_stack += chr; break;
-            case '-': op_stack += chr; break;
-                if (!op_stack.empty()){
-                    result+=op_stack.back();
-                    op_stack.erase(op_stack.end()-1);
-                }
+            case '-':
+                if (!op_stack.empty() & op_stack.back() != '(') stack_dump(result, op_stack);
                 op_stack += chr; break;
-            default: result += chr; 
+            default: result += chr;
         }
     }
-    return "";
+    if (!op_stack.empty()) stack_dump(result, op_stack);
+    return result;
 }
 
 class Assert{
@@ -68,9 +66,9 @@ T Equals(T value){
     return value;
 }
 int main(){
-    Assert::That<std::string>(to_postfix("2+7*5"), Equals<std::string>("275*+"));
-    Assert::That<std::string>(to_postfix("3*3/(7+1)"), Equals<std::string>("33*71+/"));
-    Assert::That<std::string>(to_postfix("5+(6-2)*9+3^(7-1)"), Equals<std::string>("562-9*+371-^+"));
+    // Assert::That<std::string>(to_postfix("2+7*5"), Equals<std::string>("275*+"));
+    // Assert::That<std::string>(to_postfix("3*3/(7+1)"), Equals<std::string>("33*71+/"));
+    // Assert::That<std::string>(to_postfix("5+(6-2)*9+3^(7-1)"), Equals<std::string>("562-9*+371-^+"));
     Assert::That<std::string>(to_postfix("(5-4-1)+9/5/2-7/1/7"), Equals<std::string>("54-1-95/2/+71/7/-"));
     return 0;
 }
--- python/3 kyu/min_cost_binary_search.py
+++ python/3 kyu/min_cost_binary_search.py
@@ -1,6 +1,7 @@
 # https://www.codewars.com/kata/571a7c0cf24bdf99a8000df5/train/python
 from collections import deque
 from heapq import merge
+import copy
 
 
 class Tree(object):
@@ -77,32 +78,33 @@ def cost(tree, depth=1):
 
 
 def insert_into_Tree(tree, node):
-    if node > tree.root:
+    new_tree = copy.copy(tree)
+    if node > new_tree.root:
         if not tree.right:
-            tree.right = Tree(node)
+            new_tree.right = Tree(node)
         else:
-            tree.right = insert_into_Tree(tree.right, node)
-    elif node < tree.root:
-        if not tree.left:
-            tree.left = Tree(node)
+            new_tree.right = insert_into_Tree(new_tree.right, node)
+    elif node < new_tree.root:
+        if not new_tree.left:
+            new_tree.left = Tree(node)
         else:
-            tree.left = insert_into_Tree(tree.left, node)
-    return tree
+            new_tree.left = insert_into_Tree(new_tree.left, node)
+    return new_tree
 
 
 def parse_queue(queue):
-    _, tree, node_list, _ = queue.popleft()
-    if not node_list:
-        return tree
-    sub_queue = []
-    for node in node_list:
-        new_tree = insert_into_Tree(tree, node)
-        new_node_list = node_list.copy()
-        new_node_list.remove(node)
-        current_tree = str(new_tree)
-        sub_queue.append((cost(new_tree), new_tree, new_node_list, current_tree))
-    new_queue = deque(merge(queue, sorted(sub_queue)))
-    return parse_queue(new_queue)
+    while (True):
+        _, tree, node_list, current_tree = queue.popleft()
+        if not node_list:
+            return tree
+        sub_queue = []
+        for node in node_list:
+            new_tree = insert_into_Tree(tree, node)
+            new_node_list = node_list.copy()
+            new_node_list.remove(node)
+            current_tree = str(new_tree)
+            sub_queue.append((cost(new_tree), new_tree, new_node_list, current_tree))
+        queue = deque(merge(queue, sorted(sub_queue)))
 
 
 def make_min_tree(node_list):
@@ -112,11 +114,14 @@ def make_min_tree(node_list):
     Returns a minimal cost tree of all nodes in node_list.
     '''
     queue = []
+    if len(node_list) == 1:
+        return Tree(node_list[0])
     for node in node_list:
         temp_Tree = Tree(node)
         temp_node_list = node_list.copy()
         temp_node_list.remove(node)
-        queue.append((cost(temp_Tree), temp_Tree, temp_node_list, str(temp_Tree)))
+        queue.append((cost(temp_Tree), temp_Tree,
+                     temp_node_list, str(temp_Tree)))
     queue = deque(sorted(queue))
     return parse_queue(queue)
 
@@ -129,8 +134,127 @@ e = Node('E', 8)
 tree_abcde2 = Tree(d, Tree(a, None, Tree(c, Tree(b), None)), Tree(e))
 # print(tree_abcde2)
 # print(cost(tree_abcde2))
-correct = '[[_ A:10 [[B:2] C:4 _]] D:9 [E:8]]'
-result = make_min_tree([a, b, c, d, e])
-print("result: ", result)
-print("answer: ", correct)
-print(result == correct)
+
+
+def check(x, y):
+    print("result: ", x, " cost: ", cost(x))
+    print("answer: ", y)
+    print(x == y)
+    print()
+    print()
+
+
+# check(make_min_tree([a]), '[A:10]')
+# check(make_min_tree([a, b]), '[_ A:10 [B:2]]')
+# check(make_min_tree([a, b, c]), '[_ A:10 [[B:2] C:4 _]]')
+# correct = '[[_ A:10 [[B:2] C:4 _]] D:9 [E:8]]'
+# result = make_min_tree([a, b, c, d, e])
+# print("result: ", result, " cost: ", cost(result))
+# print("answer: ", correct, " cost: ", cost(tree_abcde2))
+# print(result == correct)
+
+node_list = [
+    Node("AGL", 63558),
+    Node("AJY", 92939),
+    Node("ALD", 68666),
+    Node("AOU", 55384),
+    Node("AXJ", 68745),
+    Node("AXX", 15716),
+    Node("CBX", 82270),
+    Node("CCX", 29232),
+    Node("CFJ", 43977),
+    Node("CHU", 11403),
+    Node("CMD", 79929),
+    Node("CRB", 39788),
+    Node("CVY", 26105),
+    Node("DBD", 2319),
+    Node("DCR", 99534),
+    Node("DLV", 84675),
+    Node("DSX", 40884),
+    Node("EPK", 39181),
+    Node("ESQ", 92308),
+    Node("FFY", 66830),
+    Node("FOM", 16286),
+    Node("FQE", 43854),
+    Node("FXB", 78435),
+    Node("GDD", 128),
+    Node("GHF", 87898),
+    Node("GIE", 64680),
+    Node("GJH", 36879),
+    Node("GSL", 10382),
+    Node("GWC", 67483),
+    Node("HHL", 11876),
+    Node("HLJ", 66246),
+    Node("HQQ", 15461),
+    Node("HYR", 96279),
+    Node("IHQ", 33764),
+    Node("IUM", 16104),
+    Node("IZX", 93845),
+    Node("JJP", 98416),
+    Node("JKY", 18541),
+    Node("KBM", 34251),
+    Node("KEY", 27505),
+    Node("KGG", 39501),
+    Node("KGN", 86207),
+    Node("KJA", 82983),
+    Node("KNP", 85206),
+    Node("LMY", 46791),
+    Node("LRI", 85848),
+    Node("LWT", 53981),
+    Node("MFT", 12251),
+    Node("MRB", 1913),
+    Node("NAY", 53087),
+    Node("NRA", 94153),
+    Node("NXP", 46524),
+    Node("NYX", 22916),
+    Node("OBC", 52752),
+    Node("OBY", 46029),
+    Node("ODJ", 62971),
+    Node("OVP", 56558),
+    Node("PCF", 50446),
+    Node("PTZ", 29558),
+    Node("PVR", 64787),
+    Node("QAH", 73305),
+    Node("QYF", 49216),
+    Node("RHB", 85992),
+    Node("RLW", 92569),
+    Node("ROV", 80724),
+    Node("SCC", 92333),
+    Node("SGN", 63161),
+    Node("SOG", 54060),
+    Node("SSS", 44840),
+    Node("TFY", 40060),
+    Node("TLB", 10803),
+    Node("TWR", 32887),
+    Node("TZI", 94279),
+    Node("UEV", 31119),
+    Node("UKG", 44716),
+    Node("ULR", 76830),
+    Node("VAB", 65810),
+    Node("VJF", 58050),
+    Node("VLE", 21798),
+    Node("VWP", 80918),
+    Node("VYP", 52155),
+    Node("WDE", 44576),
+    Node("WHS", 57098),
+    Node("WTN", 85932),
+    Node("XED", 81746),
+    Node("XFF", 47653),
+    Node("XKE", 16648),
+    Node("XPA", 49553),
+    Node("XQD", 35026),
+    Node("YAJ", 15048),
+    Node("YEG", 36880),
+    Node("YJO", 28252),
+    Node("YXT", 90921),
+    Node("ZET", 13282),
+    Node("ZHQ", 32177),
+    Node("ZNA", 18419),
+    Node("ZNT", 63046),
+    Node("ZOY", 65301),
+    Node("ZYP", 56786),
+]
+from time import perf_counter
+start = perf_counter()
+print(make_min_tree(node_list[:5]))
+print(perf_counter()-start)
